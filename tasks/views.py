737b27dfde772d470e244f5b8f20b8a381e1c0b5
Jonathan Carlos Costa <jothank@hotmail.com>
2022-06-16 23:27:06 -0300

    up
    up

--- tasks/views.py
+++ tasks/views.py
@@ -5,10 +5,18 @@ from django.shortcuts import redirect, render, get_object_or_404
 from .models import Task
 from .forms import TaskForm
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 
 def taskList(request):
-    tasks = Task.objects.all().order_by('-create_at')
+    tasks_list = Task.objects.all().order_by('-create_at')
+   
+    paginator = Paginator(tasks_list, 10)
+    
+    page = request.GET.get('page')
+
+    tasks = paginator.get_page(page)
+
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
 
