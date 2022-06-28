b123431738c171e5de02301a80ec049bb7d06e3c
Jonathan Carlos Costa <jothank@hotmail.com>
2022-06-27 22:46:54 -0300

    up

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,4 +5,4 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ('title', 'description', 'user', 'done')
+        fields = ('title', 'description')
